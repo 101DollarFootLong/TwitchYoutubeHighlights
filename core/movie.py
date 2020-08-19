52f70212c033ad6481364bbcded8d23cd37576f7
101dollarfootlong <Manifest2019!>
2020-08-19 18:05:32 -0400

    Optimized the text to video rendered speed

--- core/movie.py
+++ core/movie.py
@@ -5,30 +5,34 @@ from moviepy.video.io.bindings import PIL_to_npimage
 import PIL.Image as plim
 import shutil
 
+def makeframe(t):
+    im = plim.new('RGB',(200,100))
+    draw = ImageDraw.Draw(im)
+    draw.text((50, 25), t)
+    return PIL_to_npimage(im)
+
 # Need to instal ImageMagick and add it to path 'IMAGEMAGICK_BINARY'
+# Add ffmpeg into the PATH
 clips = []
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-video_path = os.getcwd().replace('core', 'DownloadedVideos')
-print(video_path)
+os.chdir(os.getcwd().replace('core', 'DownloadedVideos'))
 
-os.chdir(video_path)
 for filename in os.listdir("."):
-    if filename.endswith(".mp4"):
-        #textclip = VideoClip(makeframe("test"), duration=36)
-        clip = VideoFileClip(filename)
+    if filename.endswith(".mp4") and "_text" not in filename:
         creator_name = filename.split("_")[0]
-        # TODO: Use PIL to speed up the text render process
-        # 5 clips: 47mins
-        #textclip = TextClip(creator_name, fontsize=60, color='white').set_pos(("right", "bottom")).set_duration(int(clip.duration))
-        
-        #final_clip = CompositeVideoClip([clip,textclip])
-        #clips.append(final_clip)
-        clips.append(clip)
+        font = 'Metropolis Black.ttf'
+        twitchstreamerstr= f"TwichStreamer{creator_name}"
+        #top_right = "x=w-tw-10:y=10"
+        bottom_right = "x=w-tw-10:y=h-th-10"
+        output_name = filename.split(".mp4")[0] + "_text.mp4" 
+        script = f'ffmpeg -i {filename} -vf drawtext="fontfile={font}:fontsize=35: fontcolor=white:{bottom_right}:text="{twitchstreamerstr}""  {output_name}"'
+        print("Running Script...")
+        os.system(script)
+        text_clip = VideoFileClip(output_name)
+        clips.append(text_clip)
 
 video = concatenate_videoclips(clips, method='compose')
 render_video = "render.mp4"
 video.write_videofile(render_video)
-shutil.move(video_path+"\\"+render_video, video_path+"\\"+"Rendered"+ "\\" + render_video)
-
-# https://gist.github.com/Zulko/06f49f075fd00e99b4e6#file-moviepy_time_accuracy-py-L33-L39
+shutil.move(render_video,"Rendered"+ "\\" + render_video)
